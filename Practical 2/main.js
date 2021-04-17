6deecef0c0669f2a59b963bf69447207c391da6d
KearneyMcilroy <78023268+KearneyMcilroy@users.noreply.github.com>
2021-04-17 15:22:46 +0100

    Add files via upload

---
+++ Practical 2/main.js
@@ -0,0 +1,14 @@
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "170px";
+}
+
+function audioVol() {
+
+	var audio = document.getElementById("musicLoop");
+	audio.volume = 0.03
+
+}	
+

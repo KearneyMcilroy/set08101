14fa6c65333387b79628681c769de5eabcd193bb
KearneyMcilroy <kearneymcilroy@hotmail.com>
2021-04-18 14:30:35 +0100

    a

--- Practical 2/main.js
+++ Practical 2/main.js
@@ -1,6 +1,7 @@
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "170px";
 }
@@ -17,19 +18,3 @@ function saveName() {
 	localStorage.setItem('userInput', input);
 	document.body.innerHTML = document.body.innerHTML.replace
 }
-=======
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-function openNav() {
-  document.getElementById("mySidenav").style.width = "170px";
-}
-
-function audioVol() {
-
-	var audio = document.getElementById("musicLoop");
-	audio.volume = 0.03
-
-}	
-
->>>>>>> origin/master

dea8080115924819ff8731c36ac49dbda4677423
KearneyMcilroy <kearneymcilroy@hotmail.com>
2021-04-26 14:52:47 +0100

    aa

--- Practical 2/main.js
+++ Practical 2/main.js
@@ -34,13 +34,13 @@ function bossText() {
 	
 	document.getElementById("storytext").innerHTML = 'The sorcerer says "I am making an undead army to destroy the world. However it doesnt matter for you. I am going to kill you myself." What do you want to do?';
 	document.getElementById("hidden").classList.remove('invisible')
-	document.getElementsById("visbtn").innerHTML = "Swing your sword"
-	document.getElementById("visbtn").setAttribute('onclick', 'changeHref()')
+	document.getElementById("visbtn").innerHTML = "Swing your sword"
+	document.getElementById("visbtn").onclick = changeHREF;
 }
 
 function changeHREF() {
+	console.log("TEST")
+	document.getElementById("visbtn").href = "https://kearneymcilroy.github.io/set08101/Practical%202/finalswordswing.html";
+	document.getElementById("hidden").href = "https://kearneymcilroy.github.io/set08101/Practical%202/finalrunback.html";
 	
-	document.getElementById("hidden").href = "https://kearneymcilroy.github.io/set08101/Practical%202/finalswordswing.html"
-	d
-	document.getElementById("visbtn").href = "https://kearneymcilroy.github.io/set08101/Practical%202/finalrunback.html"
 }
